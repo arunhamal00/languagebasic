527cbc6fe965a3de93832966f63aef9bdaec47c1
Arun Hamal <thakuriarun565@gmail.com>
2024-04-22 22:08:46 +0545

    configure ef core for sqlite db

---
+++ NETSTARTER/basics/LINQ.cs
@@ -0,0 +1,49 @@
+// Language Integrated Query
+
+// Imperative [Procedural, OOP etc.] (C, C++, C# etc.) vs Declarative [Functional] (SQL, Haskell, C#, Lisp etc. ) programming languages
+
+using System.Collections.Generic;
+using System.Linq;
+
+class LINQ
+{
+    int[] ages = [23, 45, 67, 8, 9, 12, 345, 67, 889, 34, 56, 78];
+    List<Student> students = [
+        new Student() { Name = "Ram", address = "Ktm"},
+        new Student() { Name = "Laxman", address = "Dang"},
+        new Student() { Name = "Bikendra", address = "Ktm"},
+        new Student() { Name = "Sandip", address = "Dang"},
+        new Student() { Name = "Chahana", address = "Dhangadhi"},
+    ];
+
+    // Find squares of all numbers in ages collection
+
+    public void LearnLinq()
+    {
+        // Imperative
+        List<int> sqaures = [];
+        foreach (var age in ages)
+        {
+            var square = age * age;
+            sqaures.Add(square);
+        }
+
+        // Declarative
+        var squares = ages.Select(age => age * age);    // Method syntax
+
+        var squaresAlternate = from age in ages         // Expression syntax
+                               select age * age;
+
+        // Find odd numbers within ages
+        var odds = ages.Where(x => x % 2 == 1);
+
+        // Find cubes of all even numbers within ages collection
+        var cubesOfEvens = ages
+            .Where(x => x % 2 == 0)
+            .Select(y => y * y * y);
+
+        // Find students who lives in dang and sort the result by names
+        
+    }
+
+}
