cc60a5e0818adcc17b5ac51a7b2dac339241ec9e
Arun Hamal <thakuriarun565@gmail.com>
2024-04-22 22:05:17 +0545

    discussion on properties

---
+++ NETSTARTER/basics/entrypoint.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Inheritance;
+
+// Top level statements
+Student bishnu = new();
+// Student laxman = new();
+// Student sandip = new();
+// Student rajan = new();
+// Student chahana = new();
+// Student bikendra = new();
+
+bishnu.Name = "";
+bishnu.address = "Ktm";
+bishnu.roll = 23;
+bishnu.dob = new DateTime(1990, 12, 12);
+
+// bishnu.PrintDetails();
+
+// Create class called "Animal"
+// Define at least 5 internal fields
+// Define a method to print all details of animal in descriptive format
+// In program entry point, create at least two objects of Animal
+// Initialize fields for each instance
+// Print details in concole
+
+// var rec1 = new Rectangle(5.7, 3.4);
+// var p = rec1.GetPerimeter();
+
+// var rec2 = new Rectangle(123.23, 34.3);
+// var a = rec2.GetArea();
+
+// var square1 = new Square(34.5);
+// var sa = square1.GetArea();
+
+// var circle1 = new Circle(34.5);
+// var ca = circle1.GetArea();
+
+// var sphere1 = new Sphere(34.5);
+// var ssa = sphere1.GetArea();
+// sphere1.PrintSphereInfo(34.5);
+
+// var fileio = new FileIO();
+// fileio.CreateFileWithInFolder();
+
+// Console.WriteLine("sdfsadf sadfsaf asf");
+
+
+// Student.PrintStudentType();
+// Inheritance/Polymorphism -- OOP
+
+
+ParallelAsync parallelAsync = new();
+parallelAsync.ProcessNumbers();
+
+await parallelAsync.IgniteStove();
+await parallelAsync.PutKeatleyOnWithWater();
+await parallelAsync.GrindMasala();
+
+
+
+
+
+
+
+
+
+
+
+// Generics
+// LINQ
+// Async/Await
+// Attributes
